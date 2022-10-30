d291c707eac22e2d79023e56e169f0c6ee41e0bf
PranviComputech <pranvicomputech@gmail.com>
2022-10-30 21:10:57 +0530

    initial Commit

---
+++ fetch/fetch.js
@@ -0,0 +1,29 @@
+//import modules
+const express = require('express')
+let mongodb = require('mongodb')
+//import url
+let url = require('../url')
+//create mongo client
+let mcl = mongodb.MongoClient
+//create router instance
+let router = express.Router()
+router.get("/", (req, res) => {
+    //connect to mongodb
+    mcl.connect(url, (err, conn) => {
+        if (err)
+            console.log('Error in connection:- ', err)
+        else {
+            let db = conn.db("nodedb")
+            db.collection('products').find().toArray((err, array)=>{
+                if(err)
+                    console.log('Error:- ',err)
+                else{
+                    console.log('Data Sent')
+                    res.json(array)
+                }
+            })
+        }
+    })
+})
+//export router
+module.exports = router
